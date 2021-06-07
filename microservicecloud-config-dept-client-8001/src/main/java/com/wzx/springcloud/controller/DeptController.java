45259c41af47ddb9dde9d8103cf60311c15fbcb9
zmlwzx <120101108Wzx>
2021-06-07 18:53:31 +0800

    测试

--- microservicecloud-config-dept-client-8001/src/main/java/com/wzx/springcloud/controller/DeptController.java
+++ microservicecloud-config-dept-client-8001/src/main/java/com/wzx/springcloud/controller/DeptController.java
@@ -42,7 +42,7 @@ public class DeptController {
     public Object discovery()
     {
         List<String> list = client.getServices();
-        System.out.println("**********" + list);
+//        System.out.println("**********" + list);
 
         List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
         for (ServiceInstance element : srvList) {
